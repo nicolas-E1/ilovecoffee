f9fc69e02f280c2b470c07ce8dd1df635ee4fed8
Nicolás Bracigliano <nicolas.bracigliano@estimateone.com>
2023-03-10 10:44:35 +1100

    5- Leverage Async providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,6 +6,7 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavour } from './entities/flavour.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
+import { Connection } from 'typeorm';
 
 @Injectable()
 export class CoffeeBrandsFactory {
@@ -20,12 +21,18 @@ export class CoffeeBrandsFactory {
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
-        brandsFactory.create(),
-      inject: [CoffeeBrandsFactory],
+      // Note "async" here, and Promise/Async event inside the Factory function 
+      // Could be a database connection / API call / etc
+      // In our case we're just "mocking" this type of event with a Promise
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('SELECT * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async Factory');
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ],
   exports: [CoffeesService],
