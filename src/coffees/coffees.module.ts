ddb6f4e81198a496f92651435f70ec29725399e1
Nicolás Bracigliano <nicolas.bracigliano@estimateone.com>
2023-03-10 11:41:48 +1100

    6- Create a dynamic module

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -6,7 +6,6 @@ import { Coffee } from './entities/coffee.entity';
 import { Flavour } from './entities/flavour.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
-import { Connection } from 'typeorm';
 
 @Injectable()
 export class CoffeeBrandsFactory {
@@ -21,18 +20,12 @@ export class CoffeeBrandsFactory {
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
       provide: COFFEE_BRANDS,
-      // Note "async" here, and Promise/Async event inside the Factory function 
-      // Could be a database connection / API call / etc
-      // In our case we're just "mocking" this type of event with a Promise
-      useFactory: async (connection: Connection): Promise<string[]> => {
-        // const coffeeBrands = await connection.query('SELECT * ...');
-        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
-        console.log('[!] Async Factory');
-        return coffeeBrands;
-      },
-      inject: [Connection],
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
   ],
   exports: [CoffeesService],
