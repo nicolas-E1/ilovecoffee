3d272b5de8b211206d3dd0a033d05c3142828fe8
Nicolás Bracigliano <nicolas.bracigliano@estimateone.com>
2023-03-22 10:36:57 +1100

    2- Custome environment file paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,20 @@ import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    /**
+     * To specify another path for this file,
+     * let’s pass in an options object into the forRoot() method
+     * and set the envFilePath property like so:
+     * In this example, we’re looking instead for a .environment file.
+     */
+    ConfigModule.forRoot({
+      envFilePath: '.environment',
+      /**
+       * Have ConfigModule *ignore* .env files
+       * Useful when using Provider UI's such as Heroku, etc (and they handle all ENV variables)
+       */
+      ignoreEnvFile: true,
+    }),
     CoffeesModule,
     CoffeeRatingModule,
     TypeOrmModule.forRoot({
